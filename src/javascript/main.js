fe26ab680461874996e40dba066aec051e9ec685
Christina3107 <christina.knepper@gmail.com>
2020-05-17 17:59:42 +0200

    added per capita data

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,8 +1,17 @@
+//To Do:
+//Get geolocation
+//find out current location: http://api.geonames.org/countryCode?lat=47.03&lng=10.2&username=christina3107&type=JSON
+//om platstjänster ej är aktiverade jämför med Sverige
+//Skapa slotmachine, länder = från country-json???
+
 
 import Chart from '../../node_modules/chart.js/dist/Chart.bundle.js'
-
+import * as countryData from '../../node_modules/country-json/src/country-by-population.json'
 let myChart = document.getElementById('myChart').getContext('2d');
 
+
+let population = countryData.find(x => x.country === 'Italy').population;
+console.log(population)
 /* let comparisonChart = new Chart (myChart, {
   type: 'line',
   data: {
@@ -43,7 +52,7 @@ function buildChart(Cases, Dates) {
 
 
   var settings = {
-    "url": "https://api.covid19api.com/dayone/country/norway/status/confirmed/live",
+    "url": "https://api.covid19api.com/dayone/country/italy/status/deaths/live",
     "method": "GET",
     "timeout": 0,
   };
@@ -54,8 +63,9 @@ function buildChart(Cases, Dates) {
   $.ajax(settings).done(function (response) {
     $.each(response, function() {
       console.log(this.Cases)
-      currentPlaceCases.push(this.Cases)
-      currentPlaceDates.push(this.Date)
+      let perCapita = (this.Cases/population)* 100000
+      currentPlaceCases.push(perCapita)
+      currentPlaceDates.push(this.Date.substr(0, 10))
     }),
   console.log(currentPlaceCases)
   console.log(currentPlaceDates)
