d06127a582db25b5b51fb1c119c763efbf1a9168
Christina3107 <christina.knepper@gmail.com>
2020-05-17 15:44:47 +0200

    Charts testing

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,20 +1,66 @@
-import * as test from './testmodule'
+
 import Chart from '../../node_modules/chart.js/dist/Chart.bundle.js'
 
+let myChart = document.getElementById('myChart').getContext('2d');
 
-window.alert(test)
-console.log(Chart)
+/* let comparisonChart = new Chart (myChart, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [{
+      label: 'Confirmed cases',
+      data: [
+        2,
+        15,
+        398,
+        897,
+        3567,
+        10255
+      ]
+    }]
+  },
+  options: {},
+
+}) */
+
+
+function buildChart(Cases, Dates) {
+  let comparisonChart = new Chart (myChart, {
+    type: 'line',
+    data: {
+      labels: Dates,
+      datasets: [{
+        label: 'Confirmed cases',
+        data: Cases
+      }]
+    },
+    options: {},
+  
+  })
+
+}
+
 
 
   var settings = {
-    "url": "https://api.covid19api.com/summary",
+    "url": "https://api.covid19api.com/dayone/country/norway/status/confirmed/live",
     "method": "GET",
     "timeout": 0,
   };
   
+  var currentPlaceCases = []
+  var currentPlaceDates = []
+
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    $.each(response, function() {
+      console.log(this.Cases)
+      currentPlaceCases.push(this.Cases)
+      currentPlaceDates.push(this.Date)
+    }),
+  console.log(currentPlaceCases)
+  console.log(currentPlaceDates)
+  buildChart(currentPlaceCases, currentPlaceDates)
+
   }).fail(function(response) {
     console.log(response)
   })
-
