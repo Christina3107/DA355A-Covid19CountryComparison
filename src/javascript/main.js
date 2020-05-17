7b213250ec148456ae7c705ba1c1d296cc0b2bd9
Christina3107 <christina.knepper@gmail.com>
2020-05-17 14:02:37 +0200

    minor changes

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,20 +1,10 @@
 import * as test from './testmodule'
-import Chart from '/Users/christina/Google Drive/Malmö Universitet/vt20/Flerplattformsapplikationer, vt20/Inlämningsuppgifter/Inlämningsuppgift 3/DA355A-Inl3/node_modules/chart.js/dist/Chart.js'
-//let test = require('./testmodule.js')
+import Chart from '../../node_modules/chart.js/dist/Chart.bundle.js'
+
+
 window.alert(test)
 console.log(Chart)
 
-/* import axios from '../axios';
-console.log("done")
-axios.get('https://api.covid19api.com/summary')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  }) */
 
   var settings = {
     "url": "https://api.covid19api.com/summary",
