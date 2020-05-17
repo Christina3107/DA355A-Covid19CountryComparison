19f314536c3b35dfe617ca0eabecb38299aee317
Christina3107 <christina.knepper@gmail.com>
2020-05-17 13:49:45 +0200

    changed gitignore

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,6 +1,8 @@
-let test = require('./testmodule.js')
-let axios
+import * as test from './testmodule'
+import Chart from '/Users/christina/Google Drive/Malmö Universitet/vt20/Flerplattformsapplikationer, vt20/Inlämningsuppgifter/Inlämningsuppgift 3/DA355A-Inl3/node_modules/chart.js/dist/Chart.js'
+//let test = require('./testmodule.js')
 window.alert(test)
+console.log(Chart)
 
 /* import axios from '../axios';
 console.log("done")
