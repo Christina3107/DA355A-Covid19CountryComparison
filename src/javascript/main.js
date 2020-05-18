421415668a8dc71749f4971ef00c031cc7c11801
Christina3107 <christina.knepper@gmail.com>
2020-05-18 09:40:48 +0200

    minor changes

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,8 +3,6 @@
 //find out current location: http://api.geonames.org/countryCode?lat=47.03&lng=10.2&username=christina3107&type=JSON
 //om platstjänster ej är aktiverade jämför med Sverige
 //Skapa slotmachine, länder = från country-json???
-
-
 import Chart from '../../node_modules/chart.js/dist/Chart.bundle.js'
 import * as countryData from '../../node_modules/country-json/src/country-by-population.json'
 let myChart = document.getElementById('myChart').getContext('2d');
@@ -12,26 +10,6 @@ let myChart = document.getElementById('myChart').getContext('2d');
 
 let population = countryData.find(x => x.country === 'Germany').population;
 console.log(population)
-/* let comparisonChart = new Chart (myChart, {
-  type: 'line',
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    datasets: [{
-      label: 'Confirmed cases',
-      data: [
-        2,
-        15,
-        398,
-        897,
-        3567,
-        10255
-      ]
-    }]
-  },
-  options: {},
-
-}) */
-
 
 function buildChart(Cases, Dates) {
   let comparisonChart = new Chart (myChart, {
@@ -47,30 +25,27 @@ function buildChart(Cases, Dates) {
   
   })
 
-}
+};
 
-
-
-  var settings = {
-    "url": "https://api.covid19api.com/dayone/country/germany/status/deaths/live",
-    "method": "GET",
-    "timeout": 0,
-  };
+var settings = {
+  "url": "https://api.covid19api.com/dayone/country/germany/status/deaths/live",
+  "method": "GET",
+  "timeout": 0,
+};
   
+$.ajax(settings).done(function (response) {
   let currentPlaceCases = []
   let currentPlaceDates = []
+  $.each(response, function() {
+    console.log(this.Cases)
+    let perCapita = (this.Cases/population)* 100000
+    currentPlaceCases.push(perCapita)
+    currentPlaceDates.push(this.Date.substr(0, 10))
+  }),
+console.log(currentPlaceCases)
+console.log(currentPlaceDates)
+buildChart(currentPlaceCases, currentPlaceDates)
 
-  $.ajax(settings).done(function (response) {
-    $.each(response, function() {
-      console.log(this.Cases)
-      let perCapita = (this.Cases/population)* 100000
-      currentPlaceCases.push(perCapita)
-      currentPlaceDates.push(this.Date.substr(0, 10))
-    }),
-  console.log(currentPlaceCases)
-  console.log(currentPlaceDates)
-  buildChart(currentPlaceCases, currentPlaceDates)
-
-  }).fail(function(response) {
-    console.log(response)
-  })
+}).fail(function(response) {
+  console.log(response)
+})
