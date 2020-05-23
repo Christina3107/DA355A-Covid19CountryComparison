e046829b8d1dabde4f14f43950b098b7af9209f0
Christina3107 <christina.knepper@gmail.com>
2020-05-23 20:47:55 +0200

    added geolocation feature

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,16 +1,29 @@
 //To Do:
-//Get geolocation
-//find out current location: http://api.geonames.org/countryCode?lat=47.03&lng=10.2&username=christina3107&type=JSON
-//om platstjänster ej är aktiverade jämför med Sverige
 //Skapa slotmachine, länder = från country-json???
+//kolla upp hur man bygger Chart med flera datasets
+//Styling chart
+//Info
 import Chart from '../../node_modules/chart.js/dist/Chart.bundle.js'
 import * as countryData from '../../node_modules/country-json/src/country-by-population.json'
+var currentPlace = ""
 let myChart = document.getElementById('myChart').getContext('2d');
+let options = {
+  timeout: 5000
+}
 
 
-let population = countryData.find(x => x.country === 'Germany').population;
-console.log(population)
+$(document).ready(function(){
+  console.log("Retrieving position")
+  navigator.geolocation.getCurrentPosition(success, error, options);
+});
 
+//Show Chart
+$("#getStats").on("click", function() {
+  getCovidData(currentPlace)
+  
+})
+
+//Chart builder
 function buildChart(Cases, Dates) {
   let comparisonChart = new Chart (myChart, {
     type: 'line',
@@ -27,25 +40,62 @@ function buildChart(Cases, Dates) {
 
 };
 
-var settings = {
-  "url": "https://api.covid19api.com/dayone/country/germany/status/deaths/live",
-  "method": "GET",
-  "timeout": 0,
-};
-  
-$.ajax(settings).done(function (response) {
-  let currentPlaceCases = []
-  let currentPlaceDates = []
-  $.each(response, function() {
-    console.log(this.Cases)
-    let perCapita = (this.Cases/population)* 100000
-    currentPlaceCases.push(perCapita)
-    currentPlaceDates.push(this.Date.substr(0, 10))
-  }),
-console.log(currentPlaceCases)
-console.log(currentPlaceDates)
-buildChart(currentPlaceCases, currentPlaceDates)
+//Geolocation
+function success(position) {
+  console.log("This is our position: ", position);
+  let lat = position.coords.latitude
+  let lng = position.coords.longitude
+  getCountry(lat, lng)
+}
 
-}).fail(function(response) {
-  console.log(response)
-})
+function error(err) {
+  console.warn("Something went wrong: ", err.message);
+  $("#currentLocation").text(`You have to activate location services in order to use this website. Please reload the page!`)
+
+}
+
+
+//Covid19 API call
+function getCovidData(country) {
+  $.ajax({
+    url: `https://api.covid19api.com/dayone/country/${country}/status/deaths/live`,
+    method: "GET",
+    timeout: 0
+  }).done(function (response) {
+    let cases = []
+    let dates = []
+    let population = countryData.find(x => x.country === country).population;
+    $.each(response, function() {
+      //console.log(this.Cases)
+      let perCapita = (this.Cases/population)* 100000
+      cases.push(perCapita)
+      dates.push(this.Date.substr(0, 10))
+    })
+  //console.log(currentPlaceCases)
+  //console.log(currentPlaceDates)
+  buildChart(cases, dates)
+   
+  
+  }).fail(function(response) {
+    console.log(response)
+  })
+}
+
+
+//Geolocaton API call
+function getCountry(lat, lng) {
+  let url = `http://api.geonames.org/countryCode?lat=${lat}&lng=${lng}&username=christina3107&type=JSON`
+  $.ajax({
+      url: `${url}`
+  }).done(function(data) {
+    console.log("Success: ", data)
+    $("#currentLocation").text(`Your current location is ${data.countryName}`)
+    currentPlace = data.countryName
+    $("#getStats").attr("disabled", false)
+  }).fail(function(data) {
+      console.log(data);
+  });
+}
+
+
+
