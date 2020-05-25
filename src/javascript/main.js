3b4ab3751886015a76ea6d29474321979b23ef53
Christina3107 <christina.knepper@gmail.com>
2020-05-25 11:19:38 +0200

    added functionality for handling and displaying two datasets in charts.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,11 +1,18 @@
 //To Do:
 //Skapa slotmachine, länder = från country-json???
-//kolla upp hur man bygger Chart med flera datasets
 //Styling chart
 //Info
 import Chart from '../../node_modules/chart.js/dist/Chart.bundle.js'
 import * as countryData from '../../node_modules/country-json/src/country-by-population.json'
+var chartData = {
+  type: "",
+  coronaDatasets: []
+}
+console.log(chartData)
+console.log(chartData.datasets)
+
 var currentPlace = ""
+var comparisonCountry = "Germany"
 let myChart = document.getElementById('myChart').getContext('2d');
 let options = {
   timeout: 5000
@@ -13,26 +20,74 @@ let options = {
 
 
 $(document).ready(function(){
+  //lägg till spinner
   console.log("Retrieving position")
   navigator.geolocation.getCurrentPosition(success, error, options);
 });
 
+
 //Show Chart
 $("#getStats").on("click", function() {
-  getCovidData(currentPlace)
-  
+  //töm chartData ifall man vill göra en ny..., disable button tills man har valt nytt land
+  let resultCurrentPlace = getCovidData(currentPlace)
+  let resultComparisonCountry = getCovidData(comparisonCountry)
+  let promisesComplete = $.when(resultCurrentPlace, resultComparisonCountry);
+  promisesComplete.done(function() {
+    console.log("Promises done")
+    populateChartData(resultCurrentPlace.responseJSON, currentPlace)
+    populateChartData(resultComparisonCountry.responseJSON, comparisonCountry)
+    
+  }).done(function() {
+    console.log(chartData)
+    buildChart()
+  })
+
+  //Problem: skriver ut chartData innan populate är klar, måste använda deferred? för att för köra chartBuilder när det finns data
 })
+  //$.when(getCovidData(currentPlace)).done(function() {
+  //console.log(chartData.coronaDatasets[0].cases)
+    //console.log(chartData.coronaDatasets[0])
+    //let cases = chartData.coronaDatasets[0]["cases"]
+    //let dates = chartData.coronaDatasets[0].dates
+    //buildChart(cases, dates)
+  //lägg in when --> två ajax anrop, när de är klara kalla på buildChart med data från chartData
+  
+
+function populateChartData(result, country) {
+  let population = countryData.find(x => x.country === country).population;
+  console.log(country, population)
+  let dates = []
+  let cases = []
+  $.each(result, function() {
+    let perCapita = (this.Cases/population)* 100000
+    cases.push(perCapita)
+    dates.push(this.Date.substr(0, 10))
+  })
+
+  let dataset = {
+    country: country,
+    cases: cases,
+    dates: dates
+  }
+  chartData.coronaDatasets.push(dataset)
+  }
 
 //Chart builder
-function buildChart(Cases, Dates) {
+function buildChart() {
   let comparisonChart = new Chart (myChart, {
     type: 'line',
     data: {
-      labels: Dates,
-      datasets: [{
-        label: 'Confirmed cases',
-        data: Cases
-      }]
+      labels: chartData.coronaDatasets[0].dates,
+      datasets: [
+        {
+          label: chartData.coronaDatasets[0].country,
+          data: chartData.coronaDatasets[0].cases
+        },
+        {
+          label: chartData.coronaDatasets[1].country,
+          data: chartData.coronaDatasets[1].cases
+        }
+      ]
     },
     options: {},
   
@@ -54,33 +109,14 @@ function error(err) {
 
 }
 
-
 //Covid19 API call
 function getCovidData(country) {
-  $.ajax({
+  return $.ajax({
     url: `https://api.covid19api.com/dayone/country/${country}/status/deaths/live`,
     method: "GET",
     timeout: 0
-  }).done(function (response) {
-    let cases = []
-    let dates = []
-    let population = countryData.find(x => x.country === country).population;
-    $.each(response, function() {
-      //console.log(this.Cases)
-      let perCapita = (this.Cases/population)* 100000
-      cases.push(perCapita)
-      dates.push(this.Date.substr(0, 10))
-    })
-  //console.log(currentPlaceCases)
-  //console.log(currentPlaceDates)
-  buildChart(cases, dates)
-   
-  
-  }).fail(function(response) {
-    console.log(response)
   })
 }
-
 
 //Geolocaton API call
 function getCountry(lat, lng) {
@@ -96,6 +132,4 @@ function getCountry(lat, lng) {
       console.log(data);
   });
 }
-
-
 
